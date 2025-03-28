5a252a3d1f9c7108116dfeca4c3f0d4c3bbbf950
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-03-28 20:05:03 +0530

    Fix theme toggle icon

--- app/_components/theme-toggle.tsx
+++ app/_components/theme-toggle.tsx
@@ -15,8 +15,8 @@ export default function ThemeToggle() {
       className="justify-self-end"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-0 -rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-90" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
